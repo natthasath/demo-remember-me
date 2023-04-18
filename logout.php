d5a45e28541a1cec83cae4ae76bf04f124119700
natthasath <natthasath.sak@gmail.com>
2023-04-18 10:55:30 +0700

    First Commit

--- logout.php
+++ logout.php
@@ -10,7 +10,7 @@ $_SESSION = array();
 session_destroy();
 
 // Delete the "remember_user" cookie
-setcookie('remember_user', '', time() - 3600);
+setcookie('remember_user', '', time() - 604800);
 
 // Redirect to the login page
 header('Location: index.php');
